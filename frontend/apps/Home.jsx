541311326b816c7f8df65b0f2df37a0c2095b95c
Naveen Kumar-2024-aib <naveen13524g@gmail.com>
2025-09-24 17:34:10 +0530

    refactor: simplify project structure and remove unused files
    - Updated package.json to change the main entry point and removed unnecessary scripts and dependencies.
    - Deleted reset-project.js script as it is no longer needed.
    - Removed tsconfig.json to streamline TypeScript configuration.

---
+++ frontend/apps/Home.jsx
@@ -0,0 +1,14 @@
+
+import {StyleSheet, Text, View} from 'react-native';
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to NxtGenCoders!</Text>
+    </View>
+  );
+}
+
+export default Home;
+
+const styles = StyleSheet.create({});
